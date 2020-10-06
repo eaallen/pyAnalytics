69ea0c17d66886ae57828c9df5febf6e9f7116fb
eaallen <elijah.a.allen@gmail.com>
2020-10-06 08:53:20 -0600

    cool

--- mlr.py
+++ mlr.py
@@ -7,6 +7,7 @@ import itertools
 import sklearn
 from sklearn.linear_model import LinearRegression
 # print('success')
+
 def mlr_test(string="empty"):
     """
         mlr_test
@@ -85,8 +86,19 @@ def predict(x_valid, y_valid, mlr):
     print('Model quality results:  R2:', format(R2, '0.3f'),'  MAE:', format(MAE, '0.1f'), '  RMSE:', format(RMSE,'0.1f'))
     return {'r2':R2, 'mae':MAE, 'rmse':RMSE}
 
-def buildMlr(x_train,x_valid,y_train,show_summary,rm_col_arr):
-    
+def buildMlr(x_train,x_valid,y_train,rm_col_arr,show_summary=False):
+    """
+        Description: 
+            builds and trains an MLR model. 
+        Args:
+            x_train <pandas df> -- traing x vars
+            x_valid <pandas df> -- valid x vars
+            y_train <pandas df> -- train y outcome
+            rm_col_arr <list> -- list of column names that could be removed to make the mlr model better in some way (r2, mae, rmse)
+            show_summary <bool> -- print details (default Fales) 
+        Return:
+        mlr <object> -- sklearn trained, mlr model
+    """
     # create mlr model
     mlr = LinearRegression()
     #Now we fit (train) the model using the training data. This model got trained from 
@@ -101,6 +113,19 @@ def buildMlr(x_train,x_valid,y_train,show_summary,rm_col_arr):
     return mlr
 
 def runMlr(y_train,y_valid,x_train, x_valid, rm_col_arr, show_summary=False):
+    """
+        Description: 
+            builds and trains an MLR model. 
+        Args:
+            y_train <pandas df> -- train y outcome
+            y_valid <pandas df> -- valid y outcome
+            x_train <pandas df> -- traing x vars
+            x_valid <pandas df> -- valid x vars
+            rm_col_arr <list> -- list of column names that could be removed to make the mlr model better in some way (r2, mae, rmse)
+            show_summary <bool> -- print details (default Fales) 
+        Return:
+        mlr <object> -- sklearn trained, mlr model
+    """
     _x_train = x_train.drop(columns=rm_col_arr)
     _x_valid = x_valid.drop(columns=rm_col_arr)
     mlr = buildMlr(_x_train,_x_valid,y_train,show_summary,rm_col_arr)
@@ -109,6 +134,18 @@ def runMlr(y_train,y_valid,x_train, x_valid, rm_col_arr, show_summary=False):
     return predict(_x_valid, y_valid, mlr)
 
 def mlrForTimeSeries(y_train_valid,x_train_valid,x_forecast,arr_col_rmv,show_osl=True):
+    """
+        Description: 
+            builds an mlr forcast form y_train_valid and x_train_valid. 
+        Args:
+            y_train_valid <pandas df> -- train y outcome
+            x_train_valid <pandas df> -- traing x vars
+            x_forecast <pandas df> -- x variables we will use to make the forcast
+            arr_col_rmv <list> -- list of column names that could be removed to make the mlr model better in some way (r2, mae, rmse)
+            show_osl <bool> -- print details (default Fales) 
+        Return:
+        y_pred <list> -- results from the trained model
+    """
     x_train_valid = x_train_valid.drop(columns=arr_col_rmv)
     x_forecast = x_forecast.drop(columns=arr_col_rmv)
     mlr = buildMlr(x_train_valid,x_forecast,y_train_valid,show_osl,arr_col_rmv)
@@ -122,6 +159,17 @@ def mlrForTimeSeries(y_train_valid,x_train_valid,x_forecast,arr_col_rmv,show_osl
     return y_pred
 
 def timeSeriesForecastY(x_train_valid,x_forecast,y_train_valid,arr_col_rmv):
+    """
+        Description: 
+            builds an mlr forcast form y_train_valid and x_train_valid. 
+        Args:
+            x_train_valid <pandas df> -- traing x vars
+            x_forecast <pandas df> -- x variables we will use to make the forcast
+            y_train_valid <pandas df> -- train y outcome
+            arr_col_rmv <list> -- list of column names that could be removed to make the mlr model better in some way (r2, mae, rmse)
+        Return:
+        y_pred <list> -- results from the trained model
+    """
     mlr = LinearRegression()
     mlr.fit(x_train_valid.drop(columns=arr_col_rmv), y_train_valid)
     my_forcast = mlr.predict(x_forecast.drop(columns=arr_col_rmv))
